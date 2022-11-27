5a8f81ad817536a529caa7b14e2c0be8bf771060
Andrew <asundquistdavis@gmail.com>
2022-11-27 15:19:54 -0600

    post methods working

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [x for x in request.form.values()]
+    prediction_text = " ".join([feature for feature in features])
+    return render_template('index.html', prediction_text=prediction_text, features=features)
 
 if __name__ == '__main__':
     app.run(debug=True)
