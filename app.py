fe756fb655a10e45319886e835f4714ae1ec64f8
Andrew <asundquistdavis@gmail.com>
2022-11-28 21:54:40 -0600

    Models and Scalers

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, render_template
+from pickle import load
+
+ss = load(open('Scalers/d-rfc.pkl', 'rb'))
+rfc = load(open('Models/d-rfc.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -8,7 +12,8 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [x for x in request.form.values()]
+    values = {k:v for k,v in zip(request.form.keys(), request.form.values())}
+    features = []
     prediction_text = " ".join([feature for feature in features])
     return render_template('index.html', prediction_text=prediction_text, features=features)
 
