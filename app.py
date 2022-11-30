77cb4e7a4310c42e6a41ed84bc38e0db0846f682
Andrew <asundquistdavis@gmail.com>
2022-11-29 19:44:24 -0600

    added all routes in app.py

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ rfc = load(open('Models/d-rfc.pkl', 'rb'))
 
 app = Flask(__name__)
 
+# home page with infor about project - static
 @app.route('/')
-def index():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
 
+# proposed about the data
+# TK
+
+# interactive prediction page - dynamic
 @app.route('/predict', methods=['POST'])
 def predict():
     values = {k:v for k,v in zip(request.form.keys(), request.form.values())}
@@ -17,13 +22,26 @@ def predict():
     prediction_text = " ".join([feature for feature in features])
     return render_template('index.html', prediction_text=prediction_text, features=features)
 
+# --- following '/models/' routes and for each model - all static
+# knn
 @app.route('/models/knn')
 def knn():
-    return render_template('models/knn.html')
+    return render_template('model/knn.html')
 
+# logistic regression
 @app.route('/models/logistic-regression')
 def logistic_regression():
-    return render_template()
+    return render_template('model/logistic_regression.html')
+
+# nueral net
+@app.route('models/neural_network')
+def neural_network():
+    return render_template('model/neural_network.html')
+
+# random forest classifier
+@app.route('/models/random-forest-classifier')
+def random_forest_classifier():
+    return render_template('model/random_forest.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
